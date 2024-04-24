087d4c10f37c67ccae2daea29c167007d3353d11
Marc Frances <marc.frances.t@gmail.com>
2024-04-24 20:47:24 +0200

    Activity 3.3 done

--- index.js
+++ index.js
@@ -34,6 +34,17 @@ app.get('/api/persons', (request, response) => {
   response.json(persons)
 })
 
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find((p) => p.id == id)
+
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
+})
+
 app.get('/api/info', (request, response) => {
   response.send(
     `<p>Phonebook has info for ${persons.length}</p>
